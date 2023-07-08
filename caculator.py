7a1077d17cbe06d1eae29ae9b22a88bcae32e724
GoNz0 | Mn0g1as <128939384+Mn0g1as@users.noreply.github.com>
2023-07-08 18:31:47 +0200

    Update caculator.py

--- caculator.py
+++ caculator.py
@@ -1,1 +1,17 @@
+def add(num1, num2):
+  return num1 + num2
+
+def subtract(num1, num2):
+  return num1 - num2
+
+def  multiply(num1, num2):
+   return num1 * num2
+
+def dovode(num1, num2):
+   return num1 / num2
+num1 = input('Enter the first number:')
+num2 = input ('Enter the second number:')
+
+operation = input('Enter an operation')
+
 
